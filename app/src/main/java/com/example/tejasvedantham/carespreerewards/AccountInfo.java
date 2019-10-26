7a2d8cc4c0cc24f7b2d9c8217588b3bbb3eb9dfe
Tejas Vedantham <tejas6080@gmail.com>
2019-10-26 16:36:28 -0400

    Added barcode scanning capabilities

---
+++ app/src/main/java/com/example/tejasvedantham/carespreerewards/AccountInfo.java
@@ -0,0 +1,24 @@
+package com.example.tejasvedantham.carespreerewards;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AccountInfo extends Fragment {
+
+    public AccountInfo() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_account_info, container, false);
+    }
+}
